511132cfd2c69b3fd37be21d397b0fff9727f4e3
Mae Kangsadan <maemeier@live.com>
2021-03-13 22:44:07 +0100

    handleClick function

---
+++ index.js
@@ -0,0 +1,7 @@
+//find first button and addEventListner to that button
+
+document.querySelector("button").addEventListener("click", handleClick);
+
+function handleClick() {
+  //alert("I got click");
+}
